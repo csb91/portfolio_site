41a6fdae70a6846c206793d86145545f1f58e5c1
carterbrooks <carter.brooks91@gmail.com>
2023-06-01 22:54:35 -0500

    add spotify api routes need to fix recently played item structure

--- src/lib/spotify.ts
+++ src/lib/spotify.ts
@@ -1,5 +1,3 @@
-import querystring from 'querystring';
-
 const client_id = process.env.SPOTIFY_CLIENT_ID;
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN;
@@ -10,17 +8,17 @@ const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-pla
 const RECENTLY_PLAYED_ENDPOINT = `https://api.spotify.com/v1/me/player/recently-played`;
 const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
 
-const getAccessToken = async (): Promise<any> => {
+const getAccessToken = async () => {
   const response = await fetch(TOKEN_ENDPOINT, {
     method: 'POST',
     headers: {
       Authorization: `Basic ${basic}`,
-      'Content-Type': 'application.x-www-form-urlencoded',
+      'Content-Type': 'application/x-www-form-urlencoded'
     },
-    body: querystring.stringify({
+    body: new URLSearchParams({
       grant_type: 'refresh_token',
-      refresh_token,
-    }),
+      refresh_token
+    })
   });
 
   return response.json();
---
+++ src/pages/api/spotify/recently-played.ts
@@ -0,0 +1,25 @@
+import { type NextRequest } from "next/server";
+import { getRecentlyPlayed } from "@/lib/spotify";
+
+export const config = {
+  runtime: 'edge'
+};
+
+export default async function handler(req: NextRequest) {
+  const response = await getRecentlyPlayed();
+  const { items } = await response.json();
+
+  // const tracks = items.map((track) => ({
+  //   artist: track.artists.map((_artist) => _artist.name.join(', ')),
+  //   songUrl: track.external_urls.spotify,
+  //   title: track.name
+  // }));
+
+  return new Response(JSON.stringify({ items }), {
+    status: 200,
+    headers: {
+      'content-type': 'application/json',
+      'cache-control': 'public, s-maxage=60, stale-while-revalidate=30'
+    }
+  })
+}
