20e9164457ea0d4a3b0495b3d31bea56bac8af7e
carterbrooks <carter.brooks91@gmail.com>
2023-06-02 11:44:57 -0500

    fixed recently played object formatting

--- src/pages/api/spotify/recently-played.ts
+++ src/pages/api/spotify/recently-played.ts
@@ -9,13 +9,15 @@ export default async function handler(req: NextRequest) {
   const response = await getRecentlyPlayed();
   const { items } = await response.json();
 
-  // const tracks = items.map((track) => ({
-  //   artist: track.artists.map((_artist) => _artist.name.join(', ')),
-  //   songUrl: track.external_urls.spotify,
-  //   title: track.name
-  // }));
+  const tracks = items.slice(0,10).map((track) => ({
+    artist: track.track.artists.map((_artist) => _artist.name).join(', '),
+    songUrl: track.track.external_urls.spotify,
+    title: track.track.name,
+    album: track.track.album.name,
+    albumImageUrl: track.track.album.images[1].url
+  }));
 
-  return new Response(JSON.stringify({ items }), {
+  return new Response(JSON.stringify({ tracks }), {
     status: 200,
     headers: {
       'content-type': 'application/json',
