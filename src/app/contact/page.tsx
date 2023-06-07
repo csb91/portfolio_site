0fd5d976eccb760d5cdae16d505da62232f0e451
carterbrooks <carter.brooks91@gmail.com>
2023-06-07 14:10:55 -0500

    update header on contact page add padding to spotify section for mobile

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -10,9 +10,9 @@ export default function Contact() {
       <h1 className='text-4xl text-center'>
         Get In Touch!
       </h1>
-      <p className='text-center mt-2 px-4 md:px-64'>
+      <h2 className='text-2xl text-center mt-2 px-4 md:px-64'>
         I&apos;m currently looking for new opportunities. Please feel free to reach out via the contact form or using one of the other methods listed.
-      </p>
+      </h2>
       <div className='container flex flex-col-reverse justify-evenly mx-auto border-2 rounded-md md:flex-row mt-6 mb-6 p-4 rainbow-mesh max-w-5xl shadow-lg'>
         <div className='container flex flex-col justify-center mt-6 md:mt-0 md:mb-0 md:mr-10 border-2 rounded-md md:w-96 p-4'>
           <div className='flex flex-row mx-4 mb-6'>
