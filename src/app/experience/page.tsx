e6b2c5579f9aa8d7a503508d8f5ee097c4212806
carterbrooks <carter.brooks91@gmail.com>
2023-06-07 20:47:35 -0500

    start experience page

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,3 +1,16 @@
 export default function Experience() {
-  return <div>Experience</div>
+  return (
+    <div>
+      <h1 className='text-4xl text-center'>
+        Experience
+      </h1>
+      <h2 className='text-2xl text-center mt-2'>
+        Professional Work Experience.
+      </h2>
+      <div className='container flex flex-col-reverse justify-evenly mx-auto border-2 rounded-md md:flex-row mt-6 mb-6 p-4 rainbow-mesh min-w-[1024px] shadow-lg'>
+        <div>test</div>
+        <div>test</div>
+      </div>
+    </div>
+  )
 }
