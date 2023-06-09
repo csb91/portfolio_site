f28f0d78fc50b7ee08279b39fcfc50e16162be35
carterbrooks <carter.brooks91@gmail.com>
2023-06-08 23:08:50 -0500

    add project images try to fix issue with next/link causing page to stay in same place after changing pages but appears there is unresolved dev issue

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,20 +1,43 @@
 import Link from 'next/link';
-import { getAllPosts } from "../../lib/cms";
+import Image from 'next/image';
+import Swan from '../../../public/Swan.gif'
+import TwitterOpenAI from '../../../public/gpt3-input.png'
+import BlueOcean from '../../../public/blue_ocean.png'
 
 export default async function Project() {
-  const posts = await getAllPosts()
 
   return (
-    <>
     <div>
-      {posts.map((post) =>
-        <Link href={`/projects/${post.slug}`}>
-          <div>
-            <h1>{post.title}</h1>
-          </div>
-        </Link>
-      )}
+      <h1 className='text-4xl text-center'>
+        Projects
+      </h1>
+      <h2 className='text-2xl text-center mt-2 px-4 md:px-64'>
+       Check Out Some of My Recent Work.
+      </h2>
+      <div className='mt-2 mb-4'>
+        <Image
+          src={Swan}
+          height={600}
+          width={600}
+          alt='Swan Project Walkthrough gif'
+          />
+      </div>
+      <div className='mt-2 mb-4'>
+        <Image
+          src={TwitterOpenAI}
+          height={600}
+          width={600}
+          alt='Swan Project Walkthrough gif'
+          />
+      </div>
+      <div className='mt-2 mb-4'>
+        <Image
+          src={BlueOcean}
+          height={600}
+          width={600}
+          alt='Swan Project Walkthrough gif'
+          />
+      </div>
     </div>
-    </>
   )
 }
